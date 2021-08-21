8b0a1503c27103af4d65d68b447a76430193a097
Tom McLaughlin <tom@serverlessops.io>
2021-08-21 23:49:55 +0000

    Add createJpeg as it works now.
    Come back to this later

--- src/common/common/models/photodata.py
+++ src/common/common/models/photodata.py
@@ -18,7 +18,6 @@ class PhotoData:
         '''Image S3 location'''
         bucket: str
         key: str
-
     location: S3Location
 
     @dataclass
@@ -27,5 +26,37 @@ class PhotoData:
         bucket: str
         key: str
         expiration: datetime
+    jpeg_location: Union[JpegLocation, None] = None
 
-    jpeg_location: JpegLocation
+    @dataclass
+    class ImageData:
+        '''Image data'''
+        color_space: Union[str, None] = None
+
+        @dataclass
+        class Size:
+            '''Image Size'''
+            height: int
+            width: int
+        size: Union[Size, None] = None
+
+        def __post_init__(self):
+            '''Initialize child classes'''
+            for attr in self.__dict__:
+                if isinstance(self.__getattribute__(attr), dict):
+                    if attr == 'size':
+                        self.size = PhotoData.ImageData.Size(**self.size)
+    image_data: Union[ImageData, None] = None
+
+    def __post_init__(self):
+        '''Initialize child classes'''
+        # FIXME: Can I do this better and without cases foe each class?
+        for attr in self.__dict__:
+            if isinstance(self.__getattribute__(attr), dict):
+                if attr == 'location':
+                    self.location = PhotoData.S3Location(**self.location)
+                if attr == 'jpeg_location':
+                    self.jpeg_location = PhotoData.JpegLocation(**self.jpeg_location)
+                if attr == 'image_data':
+                    self.image_data = PhotoData.ImageData(**self.image_data)
+
