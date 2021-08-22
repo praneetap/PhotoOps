8288584d3facde0dd9f97b2c42215553b3cdccd9
Tom McLaughlin <tom@serverlessops.io>
2021-08-22 20:22:07 +0000

    fix bug

--- src/common/common/models/photodataitem.py
+++ src/common/common/models/photodataitem.py
@@ -9,8 +9,10 @@ from .photodata import PhotoData
 @dataclass
 class PhotoDataItem(PhotoData):
     '''PhotoData DDB Item'''
-    pk: str
-    sk: str
+    # These need values because of our sub-classing off PhotoData which has
+    # default values.
+    pk: str = ''
+    sk: str = ''
     ttl: Optional[Union[int, None]] = None
 
     def __post_init__(self):
