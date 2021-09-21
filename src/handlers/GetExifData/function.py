5e9c08167d30dd05ea104453fc30a45812bf94fc
Tom McLaughlin <tom@serverlessops.io>
2021-09-21 18:40:43 +0000

    Add Exif extract

---
+++ src/handlers/GetExifData/function.py
@@ -0,0 +1,51 @@
+'''Fetch S3 object and return EXIF data item'''
+
+import json
+import logging
+import os
+
+from typing import Any, Dict
+
+import boto3
+import exifread
+
+from aws_lambda_powertools.utilities.data_classes import S3Event
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+# FIXME: Replace with powertools logger
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+logging.root.setLevel(logging.getLevelName(log_level))
+_logger = logging.getLogger(__name__)
+
+s3_client = boto3.client('s3')
+
+
+def _get_exif_data(s3_bucket: str, s3_object: str) -> dict:
+    '''Get EXIF data from object in S3'''
+    with open('image', 'wb+') as image:
+        s3_client.download_fileobj(s3_bucket, s3_object, image)
+        image.seek(0)
+        hdr = exifread.ExifHeader(image)
+
+    exif_data = hdr.dump_tag_values()
+    exif_data['pk'] = '{}#{}'.format(s3_bucket, s3_object)
+    exif_data['sk'] = 'exif#v0'
+
+    return exif_data
+
+
+def handler(event: Dict[str, Any], context: LambdaContext) -> dict:
+    '''Function entry'''
+    _logger.info('Event: {}'.format(json.dumps(event)))
+
+    s3_event = S3Event(event)
+
+    s3_bucket = s3_event.bucket_name
+    s3_object = s3_event.object_key
+
+    exif_data = _get_exif_data(s3_bucket, s3_object)
+
+    _logger.info('EXIF: {}'.format(exif_data))
+
+    return exif_data
+
---
+++ tests/unit/src/handlers/GetExifData/test_function.py
@@ -0,0 +1,73 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=protected-access
+'''Test IngestS3Event'''
+
+import json
+import os
+
+import boto3
+import moto
+import pytest
+
+DATA_DIR = './data'
+EVENT_DIR = os.path.join(DATA_DIR, 'events')
+IMAGE_DIR = os.path.join(DATA_DIR, 'images')
+MODEL_DIR = os.path.join(DATA_DIR, 'models')
+
+### Events
+@pytest.fixture(params=['GetExifData-event-eb.json'])
+def event(request):
+    '''Return a test event'''
+    with open(os.path.join(EVENT_DIR, request.param)) as f:
+        return json.load(f)
+
+@pytest.fixture()
+def event_schema():
+    '''Return an event schema'''
+    with open(os.path.join(EVENT_DIR, 's3-notification.schema.json')) as f:
+        return json.load(f)
+
+
+@pytest.fixture(params=['GetExifData-output.json'])
+def exif_data(request):
+    '''Return EFIX data'''
+    with open(os.path.join(EVENT_DIR, request.param)) as f:
+        return json.load(f)
+
+
+@moto.mock_s3
+@pytest.fixture()
+def s3_client():
+    '''S3 client fixture'''
+    return boto3.client('s3')
+
+@pytest.fixture()
+def func(s3_client):
+    '''
+    Function fixture
+
+    Ensures function initializes a mocked boto3
+    '''
+    import src.handlers.GetExifData.function as func
+    return func
+
+### Tests
+@moto.mock_s3
+def test_handler(event, exif_data, s3_client, func, mocker):
+    '''Call handler'''
+    s3_bucket_name = event['Records'][0]['s3']['bucket']['name']
+    s3_object_key = event['Records'][0]['s3']['object']['key']
+
+    exif_data['pk'] = '{}#{}'.format(s3_bucket_name, s3_object_key)
+    exif_data['sk'] = 'exif#v0'
+
+    s3_client.create_bucket(Bucket=s3_bucket_name)
+
+    # FIXME: How do we handle pictures?
+    with open('../../../../Pictures/Lightroom Photos/2021/09/13/_DSC0252.NEF', 'rb') as f:
+        s3_client.upload_fileobj(f, s3_bucket_name, s3_object_key)
+        f.seek(0)
+
+    resp = func.handler(event, {})
+    assert resp == exif_data
+
