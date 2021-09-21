2eeb6cd94ce1733af30db35565417bbdd65d0428
Tom McLaughlin <tom@serverlessops.io>
2021-09-21 19:06:14 +0000

    Remove MakerNote to drastically shrink data.

--- src/handlers/GetExifData/function.py
+++ src/handlers/GetExifData/function.py
@@ -28,6 +28,13 @@ def _get_exif_data(s3_bucket: str, s3_object: str) -> dict:
         hdr = exifread.ExifHeader(image)
 
     exif_data = hdr.dump_tag_values()
+
+    # MakerNote data can be big
+    if exif_data.get('IFD0') is not None:
+        if exif_data.get('IFD0').get('EXIF') is not None:
+            if exif_data.get('IFD0').get('EXIF').get('MakerNote') is not None:
+                del exif_data['IFD0']['EXIF']['MakerNote']
+
     exif_data['pk'] = '{}#{}'.format(s3_bucket, s3_object)
     exif_data['sk'] = 'exif#v0'
 
