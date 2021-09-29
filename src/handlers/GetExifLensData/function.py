e3b852c2d6d0e5a84bcb855ada0b97199c347158
Tom McLaughlin <tom@serverlessops.io>
2021-09-29 21:53:05 +0000

    dataclasses for lens data

--- src/handlers/GetExifLensData/function.py
+++ src/handlers/GetExifLensData/function.py
@@ -4,10 +4,11 @@ import json
 import logging
 import os
 
+from dataclasses import asdict
 from typing import Any, Dict, List, Optional, Union
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
-
+from common import LensExifData, LensExifDataItem, LensExifDataResponse
 
 # FIXME: Replace with powertools logger
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
@@ -15,25 +16,20 @@ logging.root.setLevel(logging.getLevelName(log_level))
 _logger = logging.getLogger(__name__)
 
 
-def _get_lens_model(event: dict) -> str:
-    '''Return lens model'''
-    pass
-
-
 def _get_lens_focal_attrs(ifd: dict) -> Dict[str, Union[int, float, None]]:
     '''Return lens focal attributes'''
     focal_attrs = ifd.get('MakerNote',{}).get('LensMinMaxFocalMaxAperture')
     return {
-        'MinFocal': int(focal_attrs[0]),
-        'MaxFocal': int(focal_attrs[1]),
-        'MinAperture': None,
-        'MaxApertureHigh': focal_attrs[2],
-        'MaxApertureLow': focal_attrs[3],
+        'min_focal': int(focal_attrs[0]),
+        'max_focal': int(focal_attrs[1]),
+        'min_aperture': None,
+        'max_aperture_high': focal_attrs[2],
+        'max_aperture_low': focal_attrs[3],
     }
 
 
 
-def _get_exif_lens_data(event: dict) -> Dict[str, Union[str, int, float, None, List[str]]]:
+def _get_exif_lens_data(event: dict) -> LensExifData:
     '''Return normalized lens data'''
     ifd = event.get('Exif', {}).get('IFD0', {})
 
@@ -41,38 +37,39 @@ def _get_exif_lens_data(event: dict) -> Dict[str, Union[str, int, float, None, L
         **_get_lens_focal_attrs(ifd)
     }
 
-    lens_data['LensMakerType'] = None
-    lens_data['CameraMakerType'] = ifd.get('MakerNote', {}).get('LensType', '').split(' ')
+    lens_data['lens_maker_type'] = []
+    lens_data['camera_maker_type'] = ifd.get('MakerNote', {}).get('LensType', '').split(' ')
 
-    lens_data['AutoFocus'] = True if lens_data['CameraMakerType'][0] == 'AF' else False
-    lens_data['VibrationReduction'] = True if 'VR' in lens_data['CameraMakerType'] else False
+    lens_data['auto_focus'] = True if lens_data['camera_maker_type'][0] == 'AF' else False
+    lens_data['vibration_reduction'] = True if 'VR' in lens_data['camera_maker_type'] else False
 
     # These are in LensData which I don't know how to read
-    lens_data['Make'] = None
-    lens_data['Model'] = None
-    lens_data['SerialNumber'] = None
-    lens_data['Macro'] = None
-    lens_data['Zoom'] = True if lens_data['MinFocal'] != lens_data['MaxFocal'] else False
+    lens_data['make'] = None
+    lens_data['model'] = None
+    lens_data['serial_number'] = None
+    lens_data['macro'] = None
+    lens_data['zoom'] = True if lens_data['min_focal'] != lens_data['max_focal'] else False
 
-    return lens_data
+    return LensExifData(**lens_data)
 
 
-def handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
+def handler(event: Dict[str, Any], context: LambdaContext) -> LensExifDataResponse:
     '''Function entry'''
     _logger.debug('Event: {}'.format(json.dumps(event)))
 
     pk = event.get('pk')
     sk = 'lens#v0'
     lens_data = _get_exif_lens_data(event)
-
-    response = {
-        'Item': {
+    lens_data_item = LensExifDataItem(
+        **{
             'pk': pk,
             'sk': sk,
-            **lens_data
+            **lens_data.__dict__
         }
-    }
+    )
 
-    _logger.debug('Response: {}'.format(json.dumps(response)))
+    response = LensExifDataResponse(**{'Item': lens_data_item})
+
+    _logger.debug('Response: {}'.format(json.dumps(asdict(response))))
 
     return response
