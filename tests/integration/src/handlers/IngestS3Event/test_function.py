4d7bca190100a9eea1b222b6e0b7e04befaa5083
Tom McLaughlin <tom@serverlessops.io>
2021-10-22 00:15:28 +0000

    Flesh out an integration test

--- tests/integration/src/handlers/IngestS3Event/test_function.py
+++ tests/integration/src/handlers/IngestS3Event/test_function.py
@@ -1,6 +1,5 @@
 '''Test IngestS3Event'''
 
-import base64
 import json
 import os
 
@@ -9,12 +8,15 @@ import boto3
 import pytest
 from aws_lambda_powertools.utilities.data_classes import S3Event
 
-from tests.unit.src.handlers.IngestS3Event.test_function import event, s3_notification
-
 STACK_NAME = 'PhotoOps-tom'
 FUNCTION_NAME = os.path.abspath(__file__).split(os.path.sep)[-2]
 
+DATA_DIR = './data'
+EVENT_DIR = os.path.join(DATA_DIR, 'events')
+IMAGE_DIR = os.path.join(DATA_DIR, 'images')
+MODEL_DIR = os.path.join(DATA_DIR, 'models')
 
+## AWS
 @pytest.fixture()
 def session():
     '''Return a boto3 session'''
@@ -31,6 +33,20 @@ def lambda_client(session):
     return session.client('lambda')
 
 
+### Events
+@pytest.fixture()
+def event():
+    '''Return a test event'''
+    with open(os.path.join(EVENT_DIR, 'IngestS3Event-event-sns.json')) as f:
+        return json.load(f)
+
+@pytest.fixture(params=['IngestS3Event-data-put.json', 'IngestS3Event-data-delete.json'])
+def s3_notification(request):
+    '''Return an S3 notification'''
+    with open(os.path.join(EVENT_DIR, request.param)) as f:
+        return json.load(f)
+
+
 def test_handler(event, s3_notification, cfn_client, lambda_client):
     '''Test handler'''
     s3_notification_event = S3Event(s3_notification)
@@ -40,7 +56,6 @@ def test_handler(event, s3_notification, cfn_client, lambda_client):
         StackName=STACK_NAME,
         LogicalResourceId=FUNCTION_NAME
     )
-
     function_name = function_info['StackResourceDetail']['PhysicalResourceId']
 
     resp = lambda_client.invoke(
@@ -48,9 +63,7 @@ def test_handler(event, s3_notification, cfn_client, lambda_client):
         LogType='Tail',
         Payload=json.dumps(event).encode('utf-8')
     )
-
     resp_body = resp.pop('Payload').read().decode()
 
-    print(json.dumps(resp, indent=4))
-    print(json.dumps(json.loads(resp_body), indent=4))
-    print(base64.b64decode(resp['LogResult']).decode())
+    assert resp['StatusCode'] == 200
+    assert json.loads(resp_body) == s3_notification
